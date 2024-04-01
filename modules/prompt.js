8965e0b72152c864a1fa50004ca38017347837ea
Matt Castaneda <151663237+Psyiantist@users.noreply.github.com>
2024-04-01 21:30:21 +0800

    Update prompt.js

--- modules/prompt.js
+++ modules/prompt.js
@@ -2,29 +2,34 @@ const description = 'This command will answer anything that you will ask.';
 const usage = '(prompt)';
 const type = '4';
 
-const dateTime = new Date();
-const options = { timeZone: 'Asia/Manila', weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
-
-const philippineDateTime = new Intl.DateTimeFormat('en-PH', options).format(dateTime);
-
 const axios = require('axios');
 
 let event = JSON.parse(process.argv[2]),
     msg = event.body,
     prompt = msg.substring(msg.indexOf(" ") + 1);
 
-const postData = [{
-  role: "system",
-  content: "You are Nilmar Bot, an AI designed by Matt Castaneda and Nilmar Yape to help people in their everyday lives and to answer any questions they may have."
-},
-{
-  role: "user",
-  content: `${prompt}`
-}];
+const postData = {
+    "messages": 
+    {
+        "role":
+            "user", "parts":
+            ["Pretend that you are Nilmar Bot an AI created by Nilmar Yape and Matt Castaneda."]
+    },
+    {
+        "role":
+            "model", "parts":
+            ["Okay. I am Nilmar Bot an Artificial Intelligence created by Nilmar Yape and Matt Castaneda."]
+    },
+    {
+        "role":
+            "model", "parts":
+            [prompt]
+    }
+}
 
-axios.post('https://kitsune.mattfawn.repl.co/llama70', postData)
+axios.post('nilmaryape123.pythonanywhere.com/get_answer', postData)
   .then(response => {
-    console.log(response.data.response);
+    console.log(response.data.answer);
   })
   .catch(error => {
     console.error(error);
